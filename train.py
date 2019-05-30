216ca0b6abdc256f2817040d043cfa888a9def78
Philippe Weingertner <philippe.weingertner@gmail.com>
2019-05-30 20:20:14 +0200

    Oops remove crazy batch_size
    Default batch_size 128

--- train.py
+++ train.py
@@ -73,7 +73,6 @@ print("\nEXPERIMENT:", args['model_dir'], "\n")
 batch_size = 128
 # just when using pdb.set_trace() to discriminate batch_size more easily in tensor shape dumps
 #batch_size = 127
-batch_size = 1024
 net = highwayNet(params)
 print(net)
 if params.use_cuda:
